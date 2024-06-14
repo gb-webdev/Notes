e958b98d329e9c0261eaf4cd6909fe9cd72c5808
Graham <blundell.gp@gmail.com>
2024-06-13 20:15:04 -0700

    added app page and styling

--- notes-app/notes-app-ui/src/App.tsx
+++ notes-app/notes-app-ui/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <form className='note-form'>
+        <input placeholder='Title' required></input>
+        <textarea placeholder='Content' rows={10} required ></textarea>
+        <button type='submit'>Add Note</button>
+      </form>
+      <div className='note-grid'>
+        <div className='note-item'>
+          <div className='notes-header'>
+            <button>X</button>
+          </div>
+          <h2>Note Title</h2>
+          <p>Note Content</p>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
