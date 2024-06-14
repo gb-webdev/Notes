46c094c2054d27d19bdd6879871431dbb5e42155
Graham <blundell.gp@gmail.com>
2024-06-13 22:51:44 -0700

    added note display logic

--- notes-app/notes-app-ui/src/App.tsx
+++ notes-app/notes-app-ui/src/App.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
+
 const App = () => {
+
+  const [notes, setNotes] = useState<
+    Note[]
+  >([
+    {
+      id: 1,
+      title: 'note title 1',
+      content: 'content text 1'
+    },
+    {
+      id: 2,
+      title: 'note title 2',
+      content: 'content text 2'
+    },
+    {
+      id: 3,
+      title: 'note title 3',
+      content: 'content text 3'
+    },
+    {
+      id: 4,
+      title: 'note title 4',
+      content: 'content text 4'
+    }
+
+  ])
+
   return (
     <div className="app-container">
       <form className='note-form'>
@@ -10,13 +43,15 @@ const App = () => {
         <button type='submit'>Add Note</button>
       </form>
       <div className='note-grid'>
+        {notes.map((note)=> (
         <div className='note-item'>
           <div className='notes-header'>
             <button>X</button>
           </div>
-          <h2>Note Title</h2>
-          <p>Note Content</p>
+          <h2>{note.title}</h2>
+          <p>{note.content}</p>
         </div>
+        ))}
       </div>
     </div>
   )
